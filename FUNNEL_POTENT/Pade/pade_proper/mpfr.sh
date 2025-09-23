fac92da20d006e90ffcf86664e43323d810fb836
christian_hpc <ticachristian20@gmail.com>
2025-09-24 00:18:39 +0800

    Removed Pade

--- FUNNEL_POTENT/Pade/pade_proper/mpfr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm Pade
-
-mpicxx -I/usr/local/include/boost/ -o Pade pade.cpp -L/usr/local/lib -O3 -lboost_mpi -lboost_serialization -lboost_thread -lboost_system -lboost_chrono -pthread -lmpfr -lgmp
-
-mpirun --oversubscribe -np 1 ./Pade
-
