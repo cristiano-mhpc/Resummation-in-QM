bc13205aa896f9055eb60129e8b42bcd88a11d05
christian_hpc <ticachristian20@gmail.com>
2025-09-24 08:47:01 +0800

    Stop tracking PT_SYm and Funnel poten but keep them locally

--- FUNNEL_POTENT/1_25kmoments_2kdig/first/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-mkdir -p build
-cmake -S . -B build -DCMAKE_CXX_COMPILER=mpicxx
-cmake --build build -j
-
-mpirun -np 5 ./build/first 
-
--- FUNNEL_POTENT/1_25kmoments_2kdig/fourth/mpfr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm fourth
-
-mpicxx -I/usr/local/include/boost/ -o fourth fourth.cpp -O3 -L/usr/local/lib -lboost_mpi -lboost_serialization -lboost_thread -lboost_system -lboost_chrono -pthread -lmpfr -lgmp
-
-mpirun --oversubscribe -np 4 ./fourth
